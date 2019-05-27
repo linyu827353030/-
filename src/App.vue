034af3473a19acb4e70158929ca57e40774a68b8
linyu827353030 <827353030@qq.com>
2019-05-27 11:12:51 +0800

    xiaofang

---
+++ src/App.vue
@@ -0,0 +1,287 @@
+<template>
+  <div>
+    <div class="app-head">
+      <div class="app-head-inner">
+        <router-link :to="{path: '/'}" class="head-logo">
+          <img src="./assets/logo.png">
+        </router-link>
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li @click="showDialog('isShowLogin')">登录</li>
+            <li class="nav-pile">|</li>
+            <li @click="showDialog('isShowReg')">注册</li>
+            <li class="nav-pile">|</li>
+            <li @click="showDialog('isShowAbout')">关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+    <div class="app-foot">
+      <p>© 2019 by Linlin</p>
+    </div>
+    <this-dialog :is-show="isShowAbout" @on-close="hideDialog('isShowAbout')">
+      <p>
+        以个人为单位，使用Dreamweaver开发工具，开发一个包含JS特效的网站，基本要求如下：
+        （一）网站主题:主题健康、积极，可以是结伴游网站、货车租赁网站、餐饮加盟店、专题站点、个人网站、班级主页、实时新闻、学习交流等等。
+        （二）根据所选主题写一份网站需求分析文档，包括编写目的，项目背景、目标、开发和运行环境、功能需求、栏目设置、各功能模块的详解等
+        。
+      </p>
+    </this-dialog>
+    <this-dialog :is-show="isShowLogin" @on-close="hideDialog('isShowLogin')">
+      <login-form @on-success @on-error></login-form>
+    </this-dialog>
+  </div>
+</template>
+
+<script>
+import ThisDialog from "./components/Dialog";
+import LoginForm from "./parts/LoginForm";
+export default {
+  components: {
+    ThisDialog,
+    LoginForm
+  },
+  data: function() {
+    return {
+      isShowAbout: false,
+      isShowLogin: false,
+      isShowReg: false
+    };
+  },
+  methods: {
+    showDialog(param) {
+      this[param] = true;
+    },
+    hideDialog(param) {
+      this[param] = false;
+    }
+  }
+};
+</script>
+
+<style>
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+body {
+  background: #f0f2f5;
+  font-family: "Helvetica Neue", Helvetica, Arial, "Hiragino Sans GB",
+    "Hiragino Sans GB W3", "Microsoft YaHei UI", "Microsoft YaHei",
+    "WenQuanYi Micro Hei", sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+.app-head {
+  background: #363636;
+  color: #b2b2b2;
+  height: 90px;
+  line-height: 90px;
+  width: 100%;
+}
+.app-head-inner {
+  width: 1200px;
+  margin: 0 auto;
+}
+.head-logo {
+  float: left;
+  margin-top: 20px;
+}
+.head-logo img {
+  width: 50px;
+}
+.head-nav {
+  float: right;
+}
+.head-nav ul {
+  overflow: hidden;
+}
+.head-nav li {
+  cursor: pointer;
+  float: left;
+}
+.nav-pile {
+  padding: 0 10px;
+}
+.app-foot {
+  text-align: center;
+  height: 80px;
+  width: 100%;
+  line-height: 80px;
+  background: #e3e4e8;
+  clear: both;
+  margin-top: 30px;
+}
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+.hr {
+  height: 1px;
+  width: 100%;
+  background: #ddd;
+}
+.button {
+  background: #4fc08d;
+  color: #fff;
+  display: inline-block;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.button:hover {
+  background: #4fc08d;
+}
+.g-form {
+}
+.g-form-line {
+  padding: 15px 0;
+}
+.g-form-label {
+  width: 100px;
+  font-size: 16px;
+  display: inline-block;
+}
+.g-form-input {
+  display: inline-block;
+}
+.g-form-input input {
+  height: 30px;
+  width: 200px;
+  line-height: 30px;
+  vertical-align: middle;
+  padding: 0 10px;
+  border: 1px solid #ccc;
+}
+.g-form-btn {
+  padding-left: 100px;
+}
+</style>
